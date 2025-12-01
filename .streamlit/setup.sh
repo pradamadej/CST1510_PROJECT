7ea1ad7e122f7a34ec64037bb60379808c41c03d
pradamadej <136495550+pradamadej@users.noreply.github.com>
2025-12-01 10:19:34 +0000

    forced updated python packages in requirements.txt

--- .streamlit/setup.sh
+++ .streamlit/setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-# Upgrade pip first
-python -m pip install --upgrade pip
+# Force Python 3.9 if available
+python --version
+
+# Upgrade pip and setuptools
+pip install --upgrade pip setuptools wheel
 
 # Install requirements
 pip install --no-cache-dir -r requirements.txt
